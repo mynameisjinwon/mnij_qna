88f926399bbcd7481f93481c85151ec7f698a2ed
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-07-14 22:00:42 +0900

    initial commit

---
+++ src/main/java/com/example/demo/MnijQnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnijQnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnijQnaApplication.class, args);
+	}
+
+}
