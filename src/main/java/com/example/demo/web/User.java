8c22b80ba732e914a0e6ec5dd7313a0c5feba32c
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-07-28 23:24:17 +0900

    UserController.java, userList.html 회원가입을 완료하면 회원 목록을 보여주는 페이지로 redirect하는 기능 추가
    form.html, User.java 파일의 변수 이름 변경 userid -> userId
    
    form.html, login.html post 방식으로 수정
    
    index.html mnij 링크 주소 변경

--- src/main/java/com/example/demo/web/User.java
+++ src/main/java/com/example/demo/web/User.java
@@ -1,13 +1,13 @@
 package com.example.demo.web;
 
 public class User {
-	private String userid;
+	private String userId;
 	private String password;
 	private String name;
 	private String email;
 
-	public void setUserid(String userid) {
-		this.userid = userid;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 	public void setPassword(String password) {
 		this.password = password;
@@ -18,9 +18,22 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
 	@Override
 	public String toString() {
-		return "User [userid=" + userid + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
 	}
 	
 	
