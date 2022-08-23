dc1e137b0485cf967f5963199447db5f3ed71656
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2022-08-23 22:16:17 +0900

    UserRepository 생성후 UserController 수정

--- src/main/java/com/mnij/web/UserController.java
+++ src/main/java/com/mnij/web/UserController.java
@@ -1,46 +1,30 @@
 package com.mnij.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mnij.domain.User;
+import com.mnij.domain.UserRepository;
 
 @Controller
 public class UserController {
 
-	private List<User> users = new ArrayList<User>();
+	@Autowired
+	private UserRepository userRepository;
 	
 	@PostMapping("/create")
 	public String create(User user) {
 		System.out.println(user);
-		users.add(user);
+		userRepository.save(user);
 		return "redirect:/list";
 	}
 	
 	@GetMapping("/list") 
 	public String userList(Model model) {
-		/* 
-		for(User user : users) {
-			String userId = user.getUserId();
-			String password = user.getPassword();
-			String name = user.getName();
-			String email = user.getEmail();
-			
-			model.addAttribute("userId", userId);
-			model.addAttribute("password", password);
-			model.addAttribute("name", name);
-			model.addAttribute("email", email);
-		}
-		이렇게 하는 줄 알았다.
-		근데 아니었다.
-		*/ 
-		//그냥 list 통째로 보내고 view에서 mustache 반복문을 통해서 출력한다.
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		return "userList";
 	}
 }
